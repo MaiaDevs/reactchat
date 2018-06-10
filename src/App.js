790e58c0ec4d9653ee8b7b194b6aaee3b3c041a5
Mai <maiasalguero@gmail.com>
2018-06-10 19:06:18 +0200

    commit 2

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import ChatRoom from './components/ChatRoom';
+
+
+
+class App extends Component {
+    render(){
+      return(
+         <div className="App">
+           <AppBar position="static" color="default">
+             <ToolBar>
+               <Typography type="title" color="inherit">
+                 Test Chat firebase
+                 </Typography>
+               </ToolBar>
+             </AppBar>
+           
+          <ChatRoom/>
+        </div>
+      )
+    }   
+  }   
+  export default App;
